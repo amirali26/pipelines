76b593d5633e893004f03697964dea4014a625e8
Amir Shojae <amirshojae@alphafx.co.uk>
2021-02-28 16:17:16 +0000

    crearted  buildtronics stack

--- bin/buildtronics-be.ts
+++ bin/buildtronics-be.ts
@@ -2,8 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { BuildtronicsBeStack } from '../lib/buildtronics-be-stack';
+import { BuildtronicsFePipeline } from '../lib/buildtronics-fePipeline-stack';
 
 const envEuWest1 = { account: '460234074473', region: 'eu-west-1' };
 
 const app = new cdk.App();
 new BuildtronicsBeStack(app, 'BuildtronicsBeStack', { env: envEuWest1 });
+new BuildtronicsFePipeline(app, 'BuildtronicsFePipeline', {env: envEuWest1});
