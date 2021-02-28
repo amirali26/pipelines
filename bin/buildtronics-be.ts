dfbfe3af677836441897a797fa5b7d68617ad5e6
Amir Shojae <amirshojae@alphafx.co.uk>
2021-02-28 16:12:04 +0000

    added region to eu-west-1 and deployment test work

--- bin/buildtronics-be.ts
+++ bin/buildtronics-be.ts
@@ -3,5 +3,7 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { BuildtronicsBeStack } from '../lib/buildtronics-be-stack';
 
+const envEuWest1 = { account: '460234074473', region: 'eu-west-1' };
+
 const app = new cdk.App();
-new BuildtronicsBeStack(app, 'BuildtronicsBeStack');
+new BuildtronicsBeStack(app, 'BuildtronicsBeStack', { env: envEuWest1 });
